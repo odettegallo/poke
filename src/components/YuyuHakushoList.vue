55fd76b887257a448d0d8f597a581f2abbcd3aa5
Odette Gallo <odette.gallo@hotmail.com>
2025-10-04 22:52:54 -0300

    List

--- src/components/YuyuHakushoList.vue
+++ src/components/YuyuHakushoList.vue
@@ -1,1 +1,21 @@
-<template></template>
+<script setup>
+const props = defineProps({
+  personajes: {
+    type: Array,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+<section>
+  <ul>
+    <li v-for="personaje in personajes" :key="personaje.id">
+      <h2>{{ personaje.nombre }}</h2>
+      <p>{{ personaje.descripcion }}</p>
+    </li>
+  </ul>
+</section>
+
+</template>
